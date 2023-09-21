678cce6d8eae5d7fde3c12949afd2ac4fc241052
Syed Shabenur Hassan <112406985+riyadh2k@users.noreply.github.com>
2023-09-21 16:23:51 +0200

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -45,14 +46,18 @@ class HomePage extends StatelessWidget {
                 child: const Text('Go to Details'),
               ),
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  final snackBar = SnackBar(
+                    content: Text('Button was clicked!'),
+                    duration: Duration(seconds: 2),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                },
                 child: const Text('Click Me'),
               ),
             ],
           ),
-          Center(
-            child: Image.network('https://picsum.photos/250?image=6'),
-          ),
+          Expanded(child: ImageSlideshow()), // Using the slideshow here
         ],
       ),
     );
@@ -60,15 +65,38 @@ class HomePage extends StatelessWidget {
 }
 
 class DetailsPage extends StatelessWidget {
-  const DetailsPage({super.key});
+  const DetailsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Details Page')),
-      body: const Center(
-        child: Text('This is the details page.'),
+      appBar: AppBar(title: Text('Details Page')),
+      body: Center(child: Text('Welcome to the Details Page!')),
+    );
+  }
+}
+
+class ImageSlideshow extends StatelessWidget {
+  final List<String> imgList = [
+    'https://picsum.photos/250?image=1',
+    'https://picsum.photos/250?image=2',
+    'https://picsum.photos/250?image=3',
+    // Add more image URLs as you want...
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      options: CarouselOptions(
+        autoPlay: true,
+        aspectRatio: 2.0,
+        enlargeCenterPage: true,
       ),
+      items: imgList.map((item) => Container(
+        child: Center(
+            child: Image.network(item, fit: BoxFit.cover, width: 1000,)
+        ),
+      )).toList(),
     );
   }
 }
